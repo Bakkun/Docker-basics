7a0be5f1a3b042387d085f49966c4d35d3a74e90
Danila <danila.antonov1999@gmail.com>
2020-09-08 11:58:30 +0400

    Multi-module project.

---
+++ server/src/main/java/com/ccb/cloudbasics/CloudBasicsWebServer.java
@@ -0,0 +1,13 @@
+package com.ccb.cloudbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBasicsWebServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBasicsWebServer.class, args);
+	}
+
+}
